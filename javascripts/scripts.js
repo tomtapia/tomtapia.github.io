dfc069f72aa2225881bbe502bae16d72c155fb65
Tomás Tapia <ttapia@kaisanetwork.net>
2024-01-13 00:49:08 -0300

    Integrated Google Analytics in index.html and scripts.js

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,5 +1,12 @@
 (() => {
   'use strict'
+  // Google Analytics
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-J6YT5K3JHY');
+
+  // Contact Form
   const forms = document.querySelectorAll('.needs-validation')
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
@@ -7,9 +14,7 @@
         event.preventDefault()
         event.stopPropagation()
       }
-
-      
-
+      // To-Do: Build a contact form with recaptcha
       form.classList.add('was-validated')
     }, false)
   })
