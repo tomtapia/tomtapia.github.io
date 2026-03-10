76c6582a5198d827b3ddadb84059ffabb7b7e4a6
Tomás Tapia <ttapia@kaisanetwork.net>
2026-03-10 12:39:10 -0300

    feat(site): load featured projects from github

--- src/main.js
+++ src/main.js
@@ -1,2 +1,5 @@
 import './assets/stylesheets/style.css';
 import './assets/javascripts/scripts.js';
+import { initFeaturedProjects } from './assets/javascripts/github-projects.js';
+
+void initFeaturedProjects();
